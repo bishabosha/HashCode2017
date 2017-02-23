c365f0483d5defd5f5f18ccef0b61a40267567b9
James Thompson <jamie.thompson@stellarise.com>
2017-02-23 18:52:28 +0000

    working parser with cmd line

--- HashCode.java
+++ HashCode.java
@@ -1,6 +1,12 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class HashCode {
-	public static void main(String[] args) {
-		System.out.println("hello world");
+	public static void main(String[] args) throws IOException {
+		Scanner in = new Scanner(new FileInputStream(new File(args[0])));
+		System.out.println(in.next());
+		in.close();
 	}
 }
